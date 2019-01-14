92e2198d99e28384fbb5353a614e856c20e2e20d
ezex87 <ezequielmiranda87@gmail.com>
2019-01-14 13:10:22 +0100

    Basic form interactive validations created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BasicForm from './form/BasicForm'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header col-md-12">
+          <h1>React form app</h1>
         </header>
+        <div className="row">
+
+          <div className="col-md-4"></div>
+
+          <div className="col-md-4 content">
+            <BasicForm/>
+          </div>
+
+          <div className="col-md-4"></div>
+          
+        </div>
       </div>
     );
   }
